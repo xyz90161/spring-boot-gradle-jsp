77e4ed5c84da32e11ba723c6526da219cc76bdb0
xyz90161 <37999158+xyz90161@users.noreply.github.com>
2018-04-02 16:54:57 +0800

    upload source code

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.java.Log;
+
+
+@Log
+@Controller
+public class TestController {
+    
+	@Autowired
+	ServletContext context;
+	
+	
+	@GetMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "/WEB-INF/jsp/index.jsp";
+    }
+	
+	
+	
+    @RequestMapping("/helloWorld")
+    public @ResponseBody String hello(){
+        return "Hello World!";
+    }
+}
